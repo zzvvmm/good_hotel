35d8bbe29e07fe7ab61289f8641b181f9ff7a217
zzvvmm <zzvvmm96@gmail.com>
2018-10-06 23:51:48 +0700

    Add migrations and search feature

---
+++ app/models/hotel.rb
@@ -0,0 +1,5 @@
+class Hotel < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search_by_hotel_name, against: [:name],
+    using: {tsearch: {dictionary: 'english', prefix: true, any_word: true}}
+end
