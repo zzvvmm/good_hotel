620c9f97fea051e8cfaeff69f2d6bb5ec9e26ba2
Hoang Manh Duy <hoangduyib96@gmail.com>
2018-10-13 16:22:13 +0000

    design page review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,5 @@
+class ReviewsController < ApplicationController
+  def review
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get  'static_pages/home'
   get 'searchs/search_result'
+  get 'reviews/review'
 end
