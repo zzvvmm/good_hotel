7ef50ee935bfff4dd74434ede55318bd09710849
Pham Thi Hanh Tuyen <phamthihanhtuyen@gmail.com>
2018-10-16 01:59:51 +0000

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 20.times do
-  Hotel.create!(name: Faker::Restaurant.name)
+  Hotel.create!(name: Faker::Restaurant.name, 
+                address: Faker::Address.street_address,
+                phone_number: Faker::PhoneNumber.cell_phone, 
+                service: Faker::Restaurant.description)
 end
