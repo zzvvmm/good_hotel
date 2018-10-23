ba251aa5023df7ce96ccb8705e4868c08b95b4e3
zzvvmm <zzvvmm96@gmail.com>
2018-10-23 11:47:00 +0700

    Fix libraries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require bootstrap
+//= require rails-ujs
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require cloudinary
 //= require_tree .
