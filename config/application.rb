f5b201403a0e569585e58419c8085030c67d8a61
manjusakajin <sakurano96@gmail.com>
2018-11-18 21:45:17 +0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Workspace
     # config.i18n.default_locale = :de
 
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
   end
