f2ae71bf1367de814853fc187e53e1fccb9ae8a9
manjusakajin <sakurano96@gmail.com>
2018-11-30 13:33:37 +0700

    fix-language

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,4 +20,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+  def after_update_path_for(resource)
+     user_path(resource)
+   end
 end
